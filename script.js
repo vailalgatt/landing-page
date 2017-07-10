cc7c1ca037a70ea119701da9d789dece8587faf6
Vail Algatt <vail.algatt@gmail.com>
2017-07-09 21:50:42 -0600

    some updates

--- script.js
+++ script.js
@@ -6,3 +6,14 @@ h1.addEventListener("input", function() {
     this.setAttribute("data-heading", this.innerText);
 });
 
+var button = document.querySelector('.button');
+button.addEventListener('click', function(event) {
+    var target = document.querySelector(button.getAttribute('data-target'));
+    if (target.style.display == "none") {
+        target.style.display = "block";
+        button.innerHTML = button.getAttribute('data-shown-text');
+    } else {
+        target.style.display = "none";
+        button.innerHTML = button.getAttribute('data-hidden-text');
+    }
+});
