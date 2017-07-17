9f21bbb7f9613e66819024d5495f1f662c2e6e83
Vail Algatt <vail.algatt@gmail.com>
2017-07-17 08:55:21 -0600

    updated piechart & added spacing between projects

--- script.js
+++ script.js
@@ -6,3 +6,38 @@ function contactForm() {
         x.style.display = 'none';
     }
 }
+
+google.charts.load('current', { 'packages': ['corechart'] });
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
+
+    var data = google.visualization.arrayToDataTable([
+        ['Language', 'Ranking in proficiency'],
+        ['Html', 90],
+        ['BOOTSTRAP', 75],
+        ['Css', 65],
+        ['Sass', 65],
+        ['JavaScript', 55],
+        ['Vue', 50],
+        ['JQuery', 35],
+        ['JSON', 20],
+        ['MongoDB', 25],
+        ['SQLite', 15],
+        ['C#', 25],
+        ['.Net', 25],
+        ['Java', 15],
+        ['Ruby', 50],
+        ['Project Management', 50],
+        ['Node.js', 45],
+        ['GitHub', 50]
+    ]);
+
+    var options = {
+        title: 'Skills Breakdown'
+    };
+
+    var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+    chart.draw(data, options);
+}
